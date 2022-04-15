7a050c4f970119c314277e4cdc5515e4131e403a
DharaSampat <103776633+DharaSampat@users.noreply.github.com>
2022-04-16 00:30:25 +0530

    fixed build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // Import dependencies
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import Webcam from "react-webcam";
 import "./App.css";
-import { nextFrame } from "@tensorflow/tfjs";
+//import { nextFrame } from "@tensorflow/tfjs";
 
 import {drawRect} from "./utilities"; 
 
@@ -71,7 +71,7 @@ function App() {
 
     }
   };
-
+  //eslint-disable-next-line
   useEffect(()=>{runCoco()},[]);
 
   return (
